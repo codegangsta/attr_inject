13fe450d16e57f88d77e4aa329c145391d78d5b0
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-03-31 23:10:39 -0700

    Refactored core_ext

--- lib/attr_inject/core_ext.rb
+++ lib/attr_inject/core_ext.rb
@@ -1,15 +1,17 @@
 class Object
+  def inject_attributes(params)
+    self.class.__inject_targets.each do |target|
+      target.apply params, self
+    end
+  end
+end
+
+class Module
   def attr_inject(attribute, options={})
     __inject_targets << Inject::Target.new(self, attribute, options)
   end
 
   def __inject_targets
     @__inject_targets ||= []
-  end
-
-  def inject_attributes(params)
-    self.class.__inject_targets.each do |target|
-      target.apply params, self
-    end
   end
 end
